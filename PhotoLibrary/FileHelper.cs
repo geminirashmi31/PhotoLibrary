e8863521fb416aa1e4211c91289f280ac4b90a9c
Rashmi <geminirashmi31@gmail.com>
2019-03-06 00:04:09 -0800

    Added PhotoLibraryManager along with unit tests. Front end to be done later.

--- PhotoLibrary/FileHelper.cs
+++ PhotoLibrary/FileHelper.cs
@@ -16,9 +16,13 @@ namespace PhotoLibrary
         /// </summary>
         /// <param name="filename">Name of the file</param>
         /// <param name="content">Content to write to the file</param>
-        public static async void WriteTextFileAsync(string filename, string content)
+        public static async Task WriteTextFileAsync(string filename, string content)
         {
-            var localFolder = ApplicationData.Current.LocalFolder;
+            var textFile = await GetFilePath(filename);
+
+            await FileIO.WriteTextAsync(textFile, content);
+
+            /*var localFolder = ApplicationData.Current.LocalFolder;
             var textFile = await localFolder.CreateFileAsync
                 (filename, CreationCollisionOption.OpenIfExists);
 
@@ -26,19 +30,28 @@ namespace PhotoLibrary
             var textWriter = new DataWriter(textStream);
             textWriter.WriteString(content);
             await textWriter.StoreAsync();
-            textStream.Dispose();
+            textStream.Dispose();*/
 
         }
 
         public static async Task<string> ReadTextFileAsync(string filename)
         {
-            StorageFolder localFolder = ApplicationData.Current.LocalFolder;
-            StorageFile textFile = await localFolder.GetFileAsync(filename);
+            var textFile = await GetFilePath(filename);
             var textStream = await textFile.OpenReadAsync();
             var textReader = new DataReader(textStream);
             var textLength = textStream.Size;
             await textReader.LoadAsync((uint)textLength);
+
             return textReader.ReadString((uint)textLength);
+        }
+
+        public static async Task<StorageFile> GetFilePath(string fileName)
+        {
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var textFile = await localFolder.CreateFileAsync
+                (fileName, CreationCollisionOption.OpenIfExists);
+
+            return textFile;
         }
     }
 }
